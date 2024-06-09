fe7a7d5dbbd8f82de0247c34a42cc31135e21663
Saikat Das <anjan.lutu@gmail.com>
2024-06-10 02:36:05 +0530

    update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -65,8 +65,8 @@ var ourClients = new Swiper(".ourClients", {
 	autoplay: {
 		delay: 0,
 		// disableOnInteraction: false,
+		reverseDirection: true,
 	},
-	reverseDirection: true,
 	slidesPerView: 5.5,
 	spaceBetween: 10,
 	speed: 8000,
@@ -113,58 +113,70 @@ $(".ourWorks2 .swiper-slide").mouseenter(function() {
 
 
   
-
-const details = gsap.utils.toArray(".desktopContentSection:not(:first-child)")
-const photos = gsap.utils.toArray(".desktopPhoto:not(:first-child)")
-
-
-gsap.set(photos, {yPercent:101})
-
-const allPhotos = gsap.utils.toArray(".desktopPhoto")
+  document.addEventListener("DOMContentLoaded", function() {
+	gsap.registerPlugin(ScrollTrigger);
+	const details = gsap.utils.toArray(".desktopContentSection:not(:first-child)")
+	const photos = gsap.utils.toArray(".desktopPhoto:not(:first-child)")
 
 
-// create
-let mm = gsap.matchMedia();
+	gsap.set(photos, {yPercent:101})
 
-// add a media query. When it matches, the associated function will run
-mm.add("(min-width: 600px)", () => {
+	const allPhotos = gsap.utils.toArray(".desktopPhoto")
 
-  // this setup code only runs when viewport is at least 600px wide
-  console.log("desktop")
+
+	// create
+	let mm = gsap.matchMedia();
+
+	// add a media query. When it matches, the associated function will run
+	mm.add("(min-width: 600px)", () => {
+
+		// this setup code only runs when viewport is at least 600px wide
+		// console.log("desktop")
+			
+		ScrollTrigger.create({
+			trigger:".gallery",
+			start:"top top",
+			end:"bottom bottom",
+			pin:".right",
+			// immediateRender: false,
+			preventOverlaps: true,
+			toggleActions:"play none none reverse",
+			markers:false
+		})
+
+		//create scrolltrigger for each details section
+		//trigger photo animation when headline of each details section 
+		//reaches 80% of window height
+		details.forEach((detail, index)=> {
+
+			let headline = detail.querySelector("h1")
+			let animation = gsap.timeline()
+			.to(photos[index], {yPercent:0})
+			.set(allPhotos[index], {autoAlpha:0})
+			ScrollTrigger.create({
+				trigger:headline,
+				start:"top 80%",
+				end:"top 50%",
+				animation:animation,
+			immediateRender: false,
+			preventOverlaps: true,
+			toggleActions:"play none none reverse",
+				scrub:true,
+				markers:false
+			})
+		})
+		
+		
 	
-  ScrollTrigger.create({
-	trigger:".gallery",
-	start:"top top",
-	end:"bottom bottom",
-	pin:".right"
-})
-
-//create scrolltrigger for each details section
-//trigger photo animation when headline of each details section 
-//reaches 80% of window height
-details.forEach((detail, index)=> {
-
-	let headline = detail.querySelector("h1")
-	let animation = gsap.timeline()
-	   .to(photos[index], {yPercent:0})
-	   .set(allPhotos[index], {autoAlpha:0})
-	ScrollTrigger.create({
-		trigger:headline,
-		start:"top 80%",
-		end:"top 50%",
-		animation:animation,
-		scrub:true,
-		markers:false
-	})
-})
-	
-	
-  
-  return () => { // optional
-    // custom cleanup code here (runs when it STOPS matching)
-	  console.log("mobile")
-  };
-});
+		return () => { // optional
+			// custom cleanup code here (runs when it STOPS matching)
+			console.log("mobile")
+		};
+	});
+	/* window.addEventListener("resize", function() {
+        ScrollTrigger.refresh();
+	}); */
+  })
 
 
 
